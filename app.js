d27299b234c8163481f31d6c0b4fb391544d8325
0xlilith <0xlilith@protonmail.com>
2021-10-18 00:23:55 -0700

    clicks check - but no functionality

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("test test")
+let music = new Audio("sauce/cozy.mp3")
+let click = new Audio("sauce/click.wav")
+let gameover = false;
+let turn = 'X';
+
+const changeTurn = () => {
+    return turn === 'X' ? 'O' : 'X';
+}
+
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    wins.forEach(e => {
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== '') ) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " - WON"
+            gameover = true;
+        }
+    })
+}
+
+music.play();
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if(boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            click.play();
+            checkWin();
+            if (gameover == false)
+                document.getElementsByClassName("info")[0].innerText = turn + "'s - TURN";
+        }
+    })
+})
